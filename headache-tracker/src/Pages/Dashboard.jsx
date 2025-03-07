97f7eb8378ced4af108e1d76a29fad65d68df761
AMorganH <AMorganHale@gmail.com>
2025-03-07 08:26:35 -0500

    more setup

---
+++ headache-tracker/src/Pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+  return <h2>You will be able to see stats and log headaches here.</h2>;
+}
+
+export default Dashboard;
---
+++ headache-tracker/src/Pages/Log-Headache.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogHeadache() {
+  return <h2>Log Your Headache Here</h2>;
+}
+
+export default LogHeadache;
